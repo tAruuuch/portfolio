55f4eea342a1961ffde7acbb29f04c4d8cdbbb18
Artemchuk Dmytro <taruuuch@gmail.com>
2017-06-12 22:16:17 +0300

    Update studiobms

--- studiobms/js/main.js
+++ studiobms/js/main.js
@@ -8,12 +8,35 @@ $(document).ready(function() {
     return false;
   });
 
+  var w_h = window.innerWidth;
+  var slides = 5;
+
+  if (w_h <= 480) {
+    slides = 2;
+  } else if (w_h <= 767) {
+    slides = 3;
+  } else if (w_h <= 979 && w_h >= 768) {
+    slides = 4;
+  } else if (w_h >= 1200) {
+    slides = 5;
+  }
+
   var mySwiper = new Swiper ('.swiper-container', {
       direction: 'horizontal',
       pagination: '.swiper-pagination',
-      slidesPerView: 5,
+      slidesPerView: slides,
       paginationClickable: true,
       spaceBetween: 30,
       loop: true
     });
+});
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+
+  if(scroll >= 400) {
+    $(".navbar").addClass("navbar-top-scroll");
+  } else {
+    $(".navbar").removeClass("navbar-top-scroll");
+  }
 });
