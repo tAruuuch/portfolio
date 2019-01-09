c812dafbf7dbb5a226a088c2a8626ff0af3eacc6
Artemchuk Dmytro <taruuuch@gmail.com>
2019-01-09 14:52:27 +0200

    Refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,112 +1,169 @@
-var browserSync   = require('browser-sync'),
-    gulp          = require('gulp'),
-    pug           = require('gulp-pug'),
-    wait          = require('gulp-wait'),
-    sass          = require('gulp-sass'),
-    cleancss      = require('gulp-clean-css'),
-    autoprefixer  = require('gulp-autoprefixer'),
-    imagemin      = require('gulp-imagemin'),
-    notify        = require('gulp-notify'),
-    rename        = require('gulp-rename'),
-    del           = require('del');
+'use strict';
 
-var config = {
-  dist: 'dist/',
-  src: 'src/',
-  pugin: 'src/pug/*.pug',
-  stylein: 'src/style/*.{sass,scss}',
-  jsin: 'src/js/**/*.js',
-  imgin: 'src/img/**/*.{jpg,jpeg,png,gif,svg,ico}',
-  fontin: 'src/fonts/**/*',
-  htmlout: 'dist/',
-  styleout: 'dist/style/',
-  jsout: 'dist/js/',
-  imgout: 'dist/img/',
-  fontout: 'dist/fonts/',
-  pugwatch: 'src/pug/**/*.pug',
-  stylewatch: 'src/style/**/*.{sass,scss}'
+const browserSync  = require('browser-sync').create(),
+      gulp         = require('gulp'),
+      pug          = require('gulp-pug'),
+      sass         = require('gulp-sass'),
+      cleancss     = require('gulp-clean-css'),
+      autoprefixer = require('gulp-autoprefixer'),
+      imagemin     = require('gulp-imagemin'),
+      notify       = require('gulp-notify'),
+      rename       = require('gulp-rename'),
+      del          = require('del'),
+      sourcemaps   = require('gulp-sourcemaps'),
+      uglify       = require('gulp-uglify'),
+      wait         = require('gulp-wait');
+
+const path = {
+	build: {
+		pug   : 'build/',
+		js    : 'build/js/',
+		style : 'build/style/',
+		image : 'build/img/',
+		font  : 'build/fonts/',
+		jslib : 'build/js/lib/',
+		csslib: 'build/style/lib/'
+	},
+	src: {
+		pug  : 'src/pug/*.pug',
+		js   : 'src/js/main.js',
+		style: 'src/sass/**/*.{sass,scss}',
+		image: 'src/img/**/*.{jpg,jpeg,png,gif,svg,ico}',
+		font : 'src/font/**/*'
+	},
+	watch: {
+		pug   : 'src/pug/**/*.pug',
+		js    : 'src/js/app.js',
+		style : 'src/sass/**/*.{sass,scss}',
+		image : 'src/img/**/*.{jpg,jpeg,png,gif,svg,ico}',
+		font  : 'src/font/**/*'
+	},
+	clean : 'build/'
 };
 
-gulp.task('browser-sync', ['sass','pug','js'], function() {
-  browserSync({
-    server: {
-      baseDir: config.dist
-    },
-    notify: true
-  });
-});
+function browserSyncServer(done) {
+	browserSync.init({
+		server: {
+			baseDir: path.build.pug
+		},
+		notify: true
+	});
+	done();
+}
 
-gulp.task('sass', function () {
-  return gulp.src(config.stylein)
-    .pipe(wait(500))
-    .pipe(sass.sync())
-    .on('error', notify.onError({
-      message: 'error: <%= error.message %>',
-      title: 'sass error'
-    }))
-    .pipe(autoprefixer({
-        browsers: ['last 21 versions'],
-        cascade: false
-    }))
-    .pipe(cleancss())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(config.styleout));
-});
+function reload(done) {
+	browserSync.reload();
+	done();
+}
 
-gulp.task('pug', function buildHTML() {
-  return gulp.src([config.pugin])
-    .pipe(pug({
-      pretty: true
-    }))
-    .on('error', notify.onError({
-      message: 'error: <%= error.message %>',
-      title: 'pug error'
-    }))
-    .pipe(gulp.dest(config.htmlout));
-});
+function jsLoad() {
+	return gulp
+		.src([path.src.js])
+		.on('error', notify.onError({
+			message: '\n<%= error.message %>',
+			title: 'SCRIPTS'
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.build.js))
+		.pipe(browserSync.stream());
+}
 
-gulp.task('js', function () {
-  return gulp.src([
-      'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/shuffle-text/build/shuffle-text.min.js',
-      config.jsin
-    ])
-    .on('error', notify.onError({
-      message: 'error: <%= error.message %>',
-      title: 'js error'
-    }))
-    .pipe(gulp.dest(config.jsout))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
+function sassLoad() {
+	return gulp
+		.src([path.src.style])
+		.pipe(wait(200))
+		.pipe(sass())
+		.on('error', notify.onError({
+			message: '\n<%= error.message %>',
+			title: 'SASS'
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer({
+			browsers: ['last 4 versions']
+		}))
+		.pipe(cleancss())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.build.style))
+		.pipe(browserSync.stream());
+}
 
-gulp.task('images', function(){
-  return gulp.src(config.imgin)
-    .pipe(imagemin({
-      optimizationLevel: 5,
-      progressive: true,
-      interlaced: true
-    }))
-    .pipe(gulp.dest(config.imgout));
-});
+function pugLoad() {
+	return gulp
+		.src([path.src.pug])
+		.pipe(pug({
+			pretty: true
+		}))
+		.on('error', notify.onError({
+			message: '\n<%= error.message %>',
+			title: 'TEMPLATE'
+		}))
+		.pipe(gulp.dest(path.build.pug))
+		.pipe(browserSync.stream());
+}
 
-gulp.task('fonts',function(){
-  return gulp.src(config.fontin)
-    .pipe(gulp.dest(config.fontout));
-});
+function imageLoad() {
+	return gulp
+		.src([path.src.image])
+		// .pipe(imagemin({
+		// 	optimizationLevel: 5,
+		// 	progressive: true,
+		// 	interlaced: true,
+		// 	svgoPlugins: [{
+		// 		removeViewBox: true
+		// 	}]
+		// }))
+		.pipe(gulp.dest(path.build.image))
+		.pipe(browserSync.stream());
+}
 
-gulp.task('clean', function() {
-  return del.sync(['dist']);
-});
+function fontLoad() {
+	return gulp
+		.src([path.src.font])
+		.pipe(gulp.dest(path.build.font))
+		.pipe(browserSync.stream());
+};
 
-gulp.task('default', ['clean'], function() {
-  gulp.start('js', 'images', 'fonts', 'browser-sync', 'watch');
-});
+function jsLibLoad() {
+	return gulp
+		.src([
+			'./node_modules/jquery/dist/jquery.min.js',
+			'./node_modules/owl.carousel/dist/owl.carousel.min.js'
+		])
+		.pipe(gulp.dest(path.build.jslib))
+		.pipe(browserSync.stream());
+}
 
-gulp.task('watch', function () {
-  gulp.watch([config.stylewatch], ['sass']).on("change", browserSync.reload)
-  gulp.watch([config.pugwatch], ['pug']).on("change", browserSync.reload)
-  gulp.watch([config.jsin], ['js']).on("change", browserSync.reload)
-  gulp.watch([config.imgin], ['image']);
-});
+function cssLibLoad() {
+	return gulp
+		.src([
+			'./node_modules/normalize.css/normalize.css',
+			'./node_modules/owl.carousel/dist/assets/owl.carousel.min.css'
+		])
+		.pipe(gulp.dest(path.build.csslib))
+		.pipe(browserSync.stream());
+}
+
+function watcher() {
+	gulp.watch(path.watch.pug, gulp.series(pugLoad, reload));
+	gulp.watch(path.watch.style, gulp.series(sassLoad, reload));
+	gulp.watch(path.watch.js, gulp.series(jsLoad, reload));
+	gulp.watch(path.watch.image, gulp.series(imageLoad, reload));
+	gulp.watch(path.watch.font, gulp.series(fontLoad, reload));
+	gulp.watch(path.build.jslib, gulp.series(jsLibLoad, reload));
+	gulp.watch(path.build.csslib, gulp.series(cssLibLoad, reload));
+}
+
+function clean() {
+	return del([path.build.pug]);
+}
+
+gulp.task('default', gulp.series(clean, pugLoad, jsLoad, sassLoad, jsLibLoad, cssLibLoad, imageLoad, fontLoad, browserSyncServer, watcher));
+gulp.task('build', gulp.series(clean, pugLoad, jsLoad, sassLoad, imageLoad, fontLoad));
